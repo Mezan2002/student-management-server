6d5f73ac1cc2318b3e2a3365273218bf9e86e7ef
mezan2002 <mezanurr009@gmail.com>
2024-05-18 01:13:50 +0600

    setting up the server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import config from "./app/config";
+import app from "./app";
+import mongoose from "mongoose";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(process.env.PORT, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
