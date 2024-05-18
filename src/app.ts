d1e2cd2383f26cbab332c4f2aabca98d60261ab4
mezan2002 <mezanurr009@gmail.com>
2024-05-18 14:46:41 +0600

    get and get single student and create student api making done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modues/student/student.route';
 
 const app: Application = express();
 
@@ -7,6 +8,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// app routes
+app.use('/api/v1/students', StudentRoutes);
+
+// Home route
 app.get('/', (req: Request, res: Response) => {
   const greeting = 'Hello World!';
   res.send(greeting);
